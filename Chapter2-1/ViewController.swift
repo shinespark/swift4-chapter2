7bf19a5e94c22ba2e2ab21223d017bf087e853b7
shinespark <shinespark64@gmail.com>
2018-04-02 12:24:15 +0900

    Initial Commit

---
+++ Chapter2-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chapter2-1
+//
+//  Created by metroid on 2018/04/02.
+//  Copyright © 2018 shinespark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
